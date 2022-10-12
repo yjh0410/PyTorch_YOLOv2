099f5b524abcac954ee55a54bc80fd298d250660
yjh0410 <1394571815@qq.com>
2022-10-12 09:54:26 +0800

    update

--- config/__init__.py
+++ config/__init__.py
@@ -2,9 +2,4 @@ from .yolov2_config import yolov2_config
 
 
 def build_model_config(args):
-    if args.version == 'voc':
-        dataset = 'voc'
-    elif args.version in ['coco', 'coco-val', 'coco-test']:
-        dataset = 'coco'
-    return yolov2_config[dataset]
-    
+    return yolov2_config[args.version]
